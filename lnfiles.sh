e2cdce88a7c3c4a0b47b2ea911e4b2d3684b40f9
nkpoid <nkpoidjp@gmail.com>
2025-06-04 01:04:42 +0900

    update lnfiles.sh

--- lnfiles.sh
+++ lnfiles.sh
@@ -1,2 +1,18 @@
-ln -s ~/dotfiles/.tigrc ~/
-ln -s ~/dotfiles/.tmux.conf ~/
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/fish
+ln -sf ~/dotfiles/config.fish ~/.config/fish/
+
+mkdir -p .config/mise
+ln -sf ~/dotfiles/mise_config.toml ~/.config/mise/config.toml
+
+if [[ "$(uname)" == "Darwin" ]] && command -v ghostty &> /dev/null; then
+  ln -sf ~/dotfiles/ghostty.yml ~/Library/Application\ Support/com.mitchellh.ghostty/config
+fi
+
+mkdir -p ~/.config/yt-dlp
+ln -sf ~/dotfiles/yt-dlp.config ~/.config/yt-dlp/config
+
+ln -sf ~/dotfiles/.tigrc ~/
+ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
