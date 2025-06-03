fa3f0d6cfa5b641f3f10799d82d688e46c12dda1
nkpoid <nkpoidjp@gmail.com>
2025-06-04 08:41:55 +0900

    fix mise config dir

--- lnfiles.sh
+++ lnfiles.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/config.fish ~/.config/fish/
 
-mkdir -p .config/mise
+mkdir -p ~/.config/mise
 ln -sf ~/dotfiles/mise_config.toml ~/.config/mise/config.toml
 
 if [[ "$(uname)" == "Darwin" ]] && command -v ghostty &> /dev/null; then
