9e530a488c07f6f656b9391e346013567c4a07f6
nkpoid <nkpoidjp@gmail.com>
2025-06-27 08:16:16 +0900

    add tools

--- lnfiles.sh
+++ lnfiles.sh
@@ -3,7 +3,8 @@
 mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/config.fish ~/.config/fish/
 
-ln -sf ~/dotfiles/mise_config.toml ~/.config/mise.toml
+mkdir -p ~/.config/mise
+ln -sf ~/dotfiles/mise_config.toml ~/.config/mise/config.toml
 
 if [[ "$(uname)" == "Darwin" ]] && command -v ghostty &> /dev/null; then
   ln -sf ~/dotfiles/ghostty.yml ~/Library/Application\ Support/com.mitchellh.ghostty/config
